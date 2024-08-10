1038b98d4a7bc57cb013db4d88a72e72abeff63a
BVenturaDev <82062400+BVenturaDev@users.noreply.github.com>
2024-08-10 01:44:24 -0400

    sub controls rough draft

--- src/entities/screen.gd
+++ src/entities/screen.gd
@@ -1,6 +1,27 @@
 @tool
 extends Node3D
+var cameras = []
+var cam_num = 0
+
+func _ready():
+	cameras.append_array($SubViewport/Node3DCameras.get_children())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$SubViewport/Node3DCameras.global_transform = global_transform
+
+func _input(event):  		
+	if event.is_action_pressed("previous_camera"):
+		if cam_num == 0:
+			cam_num = cameras.size() - 1
+		else:
+			cam_num -= 1
+		if cameras[cam_num]:
+			cameras[cam_num].current = true
+	elif event.is_action_pressed("next_camera"):
+		if cam_num == cameras.size() - 1:
+			cam_num = 0
+		else:
+			cam_num += 1
+		if cameras[cam_num]:
+			cameras[cam_num].current = true
--- src/entities/submarine.gd
+++ src/entities/submarine.gd
@@ -1,14 +1,41 @@
 extends CharacterBody3D
 
+const SPEED = 2.0
+const STOP_SPEED = 2.0
+const TURN_SPEED = 10.0
 
-const SPEED = 5.0
-const ASCENT_VELOCITY = 4.5
+@onready var player = $Player
 
 func _init():
+	pass
+	"""
 	velocity.x = randf_range(-5.0, 5.0)
 	velocity.y = randf_range(-5.0, 5.0)
 	velocity.z = randf_range(-5.0, 5.0)
 	print(velocity)
+	"""
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	var direction = global_transform.basis.get_euler() * Vector3.FORWARD
+	if Input.is_action_pressed("sub_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("sub_back"):
+		direction.z += 1
+	if Input.is_action_pressed("sub_down"):
+		direction.y -= 1
+	if Input.is_action_pressed("sub_up"):
+		direction.y += 1
+	if Input.is_action_pressed("sub_left"):
+		rotation_degrees.y -= TURN_SPEED * delta
+	if Input.is_action_pressed("sub_right"):
+		rotation_degrees.y += TURN_SPEED * delta
+	direction = direction.normalized()
+	velocity += direction * SPEED * delta
+	velocity.x = clampf(velocity.x, -3.0, 3.0)
+	velocity.y = clampf(velocity.y, -3.0, 3.0)
+	velocity.z = clampf(velocity.z, -3.0, 3.0)
+	
+	velocity.x = move_toward(velocity.x, 0, SPEED / STOP_SPEED * delta)
+	velocity.y = move_toward(velocity.y, 0, SPEED / STOP_SPEED * delta)
+	velocity.z = move_toward(velocity.z, 0, SPEED / STOP_SPEED * delta)
 	move_and_slide()
