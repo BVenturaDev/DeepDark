761fa8520a377c8e7c42aea66c4756ed7851d4ce
BVenturaDev <82062400+BVenturaDev@users.noreply.github.com>
2024-08-18 13:02:12 -0400

    credits, script, and menu

---
+++ src/gui/menu_buttons.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	pass # Replace with function body.
+
+func _on_check_box_toggled(toggled_on):
+	pass # Replace with function body.
+
+func _on_h_slider_drag_ended(value_changed):
+	pass # Replace with function body.
